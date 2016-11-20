6be33047c1145e04f3cc69937c694eae0b6ba088
Anna812 <88menyus@gmail.com>
2016-11-20 18:57:02 +0100

    Add files for Workshop36-37 about loops and java basics

---
+++ week-03/day-2/Workshop36.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Anna812 on 11/20/2016.
+ */
+public class Workshop36 {
+
+    public static void main(String[] args) {
+        // Write a program which has this output
+        // # # # # #
+        // # # # # #
+        // # # # # #
+        // # # # # #
+        // # # # # #
+        // The # character should be replacable, hence store it in a variable
+
+        for (int i = 0; i < 5; i++){
+            String hash = "#";
+            System.out.println();
+            for (int j = 0; j < 5; j++){
+                System.out.printf(" %s", hash);
+            }
+        }
+    }
+}
---
+++ week-03/day-2/Workshop37.java
@@ -0,0 +1,29 @@
+/**
+ * Created by Anna812 on 11/20/2016.
+ */
+public class Workshop37 {
+
+    public static void main(String[] args) {
+        // Write a program which has this output
+        //# # # # # # #
+        //  # # # # # # #
+        // # # # # # # #
+        //  # # # # # # #
+        // # # # # # # #
+        //  # # # # # # #
+        // # # # # # # #
+        // The # character should be replacable, hence store it in a variable
+
+        for (int i = 0; i < 7; i++){
+            String hash = "#";
+            System.out.println();
+            for (int j = 0; j < 7; j++){
+                if (i%2 == 1) {
+                    System.out.printf(" %s", hash);
+                } else {
+                    System.out.printf("%s ", hash);
+                }
+            }
+            }
+    }
+}
