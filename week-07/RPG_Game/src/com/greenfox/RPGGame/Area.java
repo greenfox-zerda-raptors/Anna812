009f40f505edc8eb0691b40cf618afee42c8672a
Anna812 <88menyus@gmail.com>
2016-12-05 16:58:05 +0100

    draw one floor Tile on frame

--- week-07/RPG_Game/src/com/greenfox/RPGGame/Area.java
+++ week-07/RPG_Game/src/com/greenfox/RPGGame/Area.java
@@ -1,10 +1,25 @@
 package com.greenfox.RPGGame;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by Anna812 on 12/5/2016.
  */
 public class Area extends JComponent {
-    
+
+    public Area() {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(this);
+        frame.setVisible(true);
+        setPreferredSize(new Dimension(720, 720));
+        frame.pack();
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+        Tile floor = new Tile("floor.png", 300, 300);
+        floor.draw(graphics);
+    }
 }
