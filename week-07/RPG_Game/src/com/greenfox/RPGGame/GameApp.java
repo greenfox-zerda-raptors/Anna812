4c9d0a056456c0fd38bfecd7b0ebf8cfdb2b8576
Anna812 <88menyus@gmail.com>
2016-12-05 17:06:54 +0100

    add Tile class and GameApp

--- week-07/RPG_Game/src/com/greenfox/RPGGame/GameApp.java
+++ week-07/RPG_Game/src/com/greenfox/RPGGame/GameApp.java
@@ -1,10 +1,18 @@
 package com.greenfox.RPGGame;
 
+import javax.swing.*;
+
 /**
  * Created by Anna812 on 12/5/2016.
  */
 public class GameApp {
-    public static void main(String[] args) {
 
-    }
+    public static void main(String[] args) {
+//        SwingUtilities.invokeLater(new Runnable() {
+//            @Override
+//            public void run() {
+                new Area();
+        }
+//        });
+//    }
 }
