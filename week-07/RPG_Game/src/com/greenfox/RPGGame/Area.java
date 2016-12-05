5a5d86e6d3eec4a55f148ea9b3423d9836d6cff0
Anna812 <88menyus@gmail.com>
2016-12-05 17:06:07 +0100

    draw floor tiles all over the frame

--- week-07/RPG_Game/src/com/greenfox/RPGGame/Area.java
+++ week-07/RPG_Game/src/com/greenfox/RPGGame/Area.java
@@ -2,11 +2,14 @@ package com.greenfox.RPGGame;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 /**
  * Created by Anna812 on 12/5/2016.
  */
 public class Area extends JComponent {
+
+    private ArrayList<Tile> tileList;
 
     public Area() {
         JFrame frame = new JFrame();
@@ -15,11 +18,20 @@ public class Area extends JComponent {
         frame.setVisible(true);
         setPreferredSize(new Dimension(720, 720));
         frame.pack();
+        tileList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 11; j++) {
+                Tile floor = new Tile("floor.png", i * 72, j * 72);
+                tileList.add(floor);
+            }
+        }
     }
 
     @Override
     public void paint(Graphics graphics) {
-        Tile floor = new Tile("floor.png", 300, 300);
-        floor.draw(graphics);
+        for(Tile temp : tileList) {
+                temp.draw(graphics);
+        }
     }
 }
+
