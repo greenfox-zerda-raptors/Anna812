fa1ecb576655f12b8ffe49e05dd45383726a05e6
Anna812 <88menyus@gmail.com>
2016-12-07 12:03:13 +0100

    implement KeyListener
    add if statements in keyPressed method for pressing the arrowkeys

--- week-07/RPG_Game/src/com/greenfox/RPGGame/Board.java
+++ week-07/RPG_Game/src/com/greenfox/RPGGame/Board.java
@@ -2,12 +2,13 @@ package com.greenfox.RPGGame;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 /**
  * Created by Anna812 on 12/5/2016.
  */
-public class Board extends JComponent {
+public class Board extends JComponent implements KeyListener{
     private Area area;
     private Hero hero;
 
@@ -30,5 +31,29 @@ public class Board extends JComponent {
         }
         hero.draw(graphics);
     }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_UP) {
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+
 }
 
