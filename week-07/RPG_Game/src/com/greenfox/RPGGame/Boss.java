923fc1fcfe4a3380976f96bb518fb8781412658c
Anna812 <88menyus@gmail.com>
2016-12-08 13:37:22 +0100

    update constructor of Skeleton and Boss Class

--- week-07/RPG_Game/src/com/greenfox/RPGGame/Boss.java
+++ week-07/RPG_Game/src/com/greenfox/RPGGame/Boss.java
@@ -3,8 +3,9 @@ package com.greenfox.RPGGame;
 /**
  * Created by Anna812 on 12/5/2016.
  */
-public class Boss extends Monsters {
-    public Boss(String filename, int posX, int posY) {
-        super(filename, posX, posY);
+public class Boss extends Monster {
+
+    public Boss(int posX, int posY) {
+        super("boss.png", posX, posY);
     }
 }
--- week-07/RPG_Game/src/com/greenfox/RPGGame/Skeleton.java
+++ week-07/RPG_Game/src/com/greenfox/RPGGame/Skeleton.java
@@ -3,8 +3,9 @@ package com.greenfox.RPGGame;
 /**
  * Created by Anna812 on 12/5/2016.
  */
-public class Skeleton extends Monsters{
-    public Skeleton(String filename, int posX, int posY) {
-        super(filename, posX, posY);
+public class Skeleton extends Monster {
+
+    public Skeleton(int posX, int posY) {
+        super("skeleton.png", posX, posY);
     }
 }
