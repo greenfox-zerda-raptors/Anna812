43d09bd4b935e5f03469fc0d1afd71ed683547b6
Anna812 <88menyus@gmail.com>
2016-12-08 22:01:48 +0100

    add method to generate level of Enemy

--- week-07/RPG_Game/src/com/greenfox/RPGGame/Enemy.java
+++ week-07/RPG_Game/src/com/greenfox/RPGGame/Enemy.java
@@ -9,5 +9,17 @@ public abstract class Enemy extends PlayableCharacters {
 
     public Enemy(String filename, int posX, int posY) {
         super(filename, posX, posY);
+        level = generateLevel();
+    }
+
+    private int generateLevel() {
+        int roll = dice.nextInt(10);
+        if(roll < 5) {
+            return 1;
+        } else if(roll < 9) {
+            return 2;
+        } else {
+            return 3;
+        }
     }
 }
