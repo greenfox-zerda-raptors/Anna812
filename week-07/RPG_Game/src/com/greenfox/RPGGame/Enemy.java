5a98fd131883cec37de920cea425919ca3d26b01
Anna812 <88menyus@gmail.com>
2016-12-09 10:42:33 +0100

    change due to renaming PlayableCharacter

--- week-07/RPG_Game/src/com/greenfox/RPGGame/Enemy.java
+++ week-07/RPG_Game/src/com/greenfox/RPGGame/Enemy.java
@@ -1,11 +1,9 @@
 package com.greenfox.RPGGame;
 
-import java.util.Random;
-
 /**
  * Created by Anna on 12/7/2016.
  */
-public abstract class Enemy extends PlayableCharacters {
+public abstract class Enemy extends PlayableCharacter {
 
     public Enemy(String filename, int posX, int posY) {
         super(filename, posX, posY);
--- week-07/RPG_Game/src/com/greenfox/RPGGame/Hero.java
+++ week-07/RPG_Game/src/com/greenfox/RPGGame/Hero.java
@@ -3,7 +3,7 @@ package com.greenfox.RPGGame;
 /**
  * Created by Anna812 on 12/5/2016.
  */
-public class Hero extends PlayableCharacters {
+public class Hero extends PlayableCharacter {
 
     public Hero(String filename, int posX, int posY) {
         super(filename, posX, posY);
