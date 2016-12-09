2311eaad67bae483b040dfdfdcf342723c039653
Anna812 <88menyus@gmail.com>
2016-12-09 11:51:06 +0100

    tilt moving when hero is dead

--- week-07/RPG_Game/src/com/greenfox/RPGGame/Hero.java
+++ week-07/RPG_Game/src/com/greenfox/RPGGame/Hero.java
@@ -20,28 +20,28 @@ public class Hero extends PlayableCharacter {
     }
 
     public void moveLeft(Area area) {
-        if (area.isFloor(posX - 1, posY)) {
+        if (area.isFloor(posX - 1, posY) && isAlive()) {
             posX -= 1;
             drawImageOfObject("hero-left.png");
         }
     }
 
     public void moveRight(Area area) {
-        if (area.isFloor(posX + 1, posY)) {
+        if (area.isFloor(posX + 1, posY) && isAlive()) {
             posX += 1;
             drawImageOfObject("hero-right.png");
         }
     }
 
     public void moveUp(Area area) {
-        if (area.isFloor(posX, posY - 1)) {
+        if (area.isFloor(posX, posY - 1) && isAlive()) {
             posY -= 1;
             drawImageOfObject("hero-up.png");
         }
     }
 
     public void moveDown(Area area) {
-        if (area.isFloor(posX, posY + 1)) {
+        if (area.isFloor(posX, posY + 1) && isAlive()) {
             posY += 1;
             drawImageOfObject("hero-down.png");
         }
