f23c671a4a1eea53c673d968d0c824fd8f04b102
Anna812 <88menyus@gmail.com>
2016-12-14 15:28:28 +0100

    add Workshop03.java

---
+++ week-08/day-3/JDBC_Practice/src/Workshop03.java
@@ -0,0 +1,36 @@
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+import java.sql.SQLException;
+/**
+ * Created by Anna on 16/12/14.
+ */
+public class Workshop03 {
+
+    public static void main(String[] args) throws SQLException {
+        String databaseUrl = "jdbc:mysql://localhost:3306/Workshop03?user=root&password=0000";
+
+        ConnectionSource connectionSource = new JdbcConnectionSource(databaseUrl);
+
+        TableUtils.createTableIfNotExists(connectionSource, Account.class);
+        TableUtils.createTableIfNotExists(connectionSource, Address.class);
+
+        Dao<Account, String> accountDao =
+                DaoManager.createDao(connectionSource, Account.class);
+
+
+        Account account = new Account("Captain America", "uejnsd632**234.");
+        createAccountIfNotExists(accountDao, account);
+
+        account = accountDao.queryForId("Captain America");
+        System.out.println("Account: " + account.toString());
+    }
+
+    private static void createAccountIfNotExists(Dao<Account, String> accountDao, Account acc) throws SQLException {
+        if(accountDao.queryForId(acc.getName()) == null) {
+            accountDao.create(acc);
+        }
+    }
+}
