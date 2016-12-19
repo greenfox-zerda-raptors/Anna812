494876286982eddd57d2e4fae570f1fb7ab417e1
Anna812 <88menyus@gmail.com>
2016-12-19 16:23:15 +0100

    upload Web Greet counter app

--- week-09/day-1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-09/day-1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,11 +1,5 @@
 package com.greenfoxacademy.springstart.controllers;
-
-import com.greenfoxacademy.springstart.controllers.Greeting;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
--- week-09/day-1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-09/day-1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -3,17 +3,22 @@ package com.greenfoxacademy.springstart.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by Anna on 16/12/19.
  */
 @Controller
 public class HelloWebController {
+    private AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/web/greeting")
-    public String greeting(Model model) {
-        model.addAttribute("name", " World");
+    public String greeting(@RequestParam String name, Model model) {
+        long id = counter.incrementAndGet();
+        model.addAttribute("name", name);
+        model.addAttribute("id", id);
         return "greeting";
     }
 }
