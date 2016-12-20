43c57b1e585f30475507c9a4414f383a6943859f
Anna812 <88menyus@gmail.com>
2016-12-20 16:22:54 +0100

    refactor ToDo class to Todo, create constructors, getters and setters

--- week-09/day-2/thymeleafExercise/src/main/java/com/greenfoxacademy/ToDo.java
+++ week-09/day-2/thymeleafExercise/src/main/java/com/greenfoxacademy/ToDo.java
@@ -1,11 +1,23 @@
 package com.greenfoxacademy;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Created by Anna on 16/12/20.
  */
-public class ToDo {
+@AllArgsConstructor
+public class Todo {
+    @Getter
+    @Setter
     int id;
     String title;
     Boolean isUrgent;
     Boolean isDone;
+
+    public Todo(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
 }
