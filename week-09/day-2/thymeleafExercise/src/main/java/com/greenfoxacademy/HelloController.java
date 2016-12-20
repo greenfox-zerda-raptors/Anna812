c0311b39bd4a68575b6a5fb42c151752d279d99f
Anna812 <88menyus@gmail.com>
2016-12-20 16:07:28 +0100

    upload solution for exercise 'hello templates'

---
+++ week-09/day-2/thymeleafExercise/src/main/java/com/greenfoxacademy/HelloController.java
@@ -0,0 +1,24 @@
+package com.greenfoxacademy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Anna on 16/12/20.
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping(value = "/helloworld")
+    @ResponseBody
+    public String helloWorld(@RequestParam(value = "name", required = false, defaultValue = "Thymeleaf") String name) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd. HH:mm");
+        Date currentTime = new Date();
+        return String.format("Hello %s! %s", name, sdf.format(currentTime));
+    }
+}
---
+++ week-09/day-2/thymeleafExercise/src/main/java/com/greenfoxacademy/ThymeleafExerciseApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafExerciseApplication.class, args);
+	}
+}
