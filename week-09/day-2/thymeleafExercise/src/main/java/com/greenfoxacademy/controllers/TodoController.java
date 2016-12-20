977d6e1ea2cb27c4a11dcf8c4923728f25e06cb8
Anna812 <88menyus@gmail.com>
2016-12-20 16:16:08 +0100

    create new method in TodoController which maps to /list

--- week-09/day-2/thymeleafExercise/src/main/java/com/greenfoxacademy/controllers/TodoController.java
+++ week-09/day-2/thymeleafExercise/src/main/java/com/greenfoxacademy/controllers/TodoController.java
@@ -2,6 +2,7 @@ package com.greenfoxacademy.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Anna on 16/12/20.
@@ -9,4 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value = "/todo")
 public class TodoController {
+
+    @RequestMapping(value = "/list")
+    @ResponseBody
+    public void listToDos() {
+
+    }
 }
