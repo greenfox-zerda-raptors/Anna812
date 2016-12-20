6d3dc71e9c5ef355c4ba4ff74a4425be179c3e4c
Anna812 <88menyus@gmail.com>
2016-12-20 16:13:57 +0100

    add HelloController and TodoController to controllers package

--- week-09/day-2/thymeleafExercise/src/main/java/com/greenfoxacademy/controllers/HelloController.java
+++ week-09/day-2/thymeleafExercise/src/main/java/com/greenfoxacademy/controllers/HelloController.java
@@ -1,4 +1,4 @@
-package com.greenfoxacademy;
+package com.greenfoxacademy.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ week-09/day-2/thymeleafExercise/src/main/java/com/greenfoxacademy/controllers/TodoController.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Anna on 16/12/20.
+ */
+@Controller
+@RequestMapping(value = "/todo")
+public class TodoController {
+}
