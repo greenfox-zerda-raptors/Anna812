7d8de04ca02d54f094d6f3d01ad49f1fe8d5a687
Anna812 <88menyus@gmail.com>
2017-01-05 20:58:13 +0100

    create logic to list todos

--- week-09/day-2/thymeleafExercise/src/main/java/com/greenfoxacademy/controllers/TodoController.java
+++ week-09/day-2/thymeleafExercise/src/main/java/com/greenfoxacademy/controllers/TodoController.java
@@ -5,7 +5,9 @@ import com.greenfoxacademy.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -27,16 +29,26 @@ public class TodoController {
     @RequestMapping(value = "/list")
     public String listToDos(@RequestParam(required = false) String active, Model model) {
         if (active != null) {
-            ArrayList<Todo> activeTodos = new ArrayList<>();
-            for (Todo temp : todoService.getTodos()) {
-                if(temp.isDone()) {
-                    activeTodos.add(temp);
-                }
-            }
-            model.addAttribute("todoList", activeTodos);
+            model.addAttribute("todoList", listActiveTodos());
         } else {
             model.addAttribute("todoList", todoService.getTodos());
         }
         return "todo";
+    }
+
+    public ArrayList<Todo> listActiveTodos() {
+        ArrayList<Todo> activeTodos = new ArrayList<>();
+        for (Todo temp : todoService.getTodos()) {
+            if(temp.isDone()) {
+                activeTodos.add(temp);
+            }
+        }
+        return activeTodos;
+    }
+
+    @RequestMapping(value = "/details/{id}", method = RequestMethod.GET)
+    public String detailToDos(@PathVariable("id") int index, Model model) {
+        model.addAttribute("todo", todoService.getTodos().get(index));
+        return "details";
     }
 }
