1f4521ff143212ad2a4c6a6ea8f1281de589dcca
Anna812 <88menyus@gmail.com>
2017-01-10 13:58:39 +0100

    change /save endpoint to have a POST method instead of GET

--- week-09/CalorieCounter/src/main/java/com/greenfox/caloriecounter/controllers/CalorieCounterController.java
+++ week-09/CalorieCounter/src/main/java/com/greenfox/caloriecounter/controllers/CalorieCounterController.java
@@ -6,7 +6,9 @@ import com.greenfox.caloriecounter.services.MealTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -33,7 +35,7 @@ public class CalorieCounterController {
         return "add";
     }
 
-    @RequestMapping("/save")
+    @PostMapping("/save")
     public String save(@ModelAttribute Meal meal) {
         mealService.saveMeal(meal);
         return "redirect:/index";
