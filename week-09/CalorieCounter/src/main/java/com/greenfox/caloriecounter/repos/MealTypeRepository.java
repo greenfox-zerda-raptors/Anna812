bee208a9a11f65a426e636d72439736c36021a5b
Anna812 <88menyus@gmail.com>
2017-01-10 12:25:15 +0100

    create MealTypeRepository and MealTypeService

---
+++ week-09/CalorieCounter/src/main/java/com/greenfox/caloriecounter/repos/MealTypeRepository.java
@@ -0,0 +1,10 @@
+package com.greenfox.caloriecounter.repos;
+
+import com.greenfox.caloriecounter.models.MealType;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Anna on 17/01/10.
+ */
+public interface MealTypeRepository extends CrudRepository<MealType, Long>{
+}
---
+++ week-09/CalorieCounter/src/main/java/com/greenfox/caloriecounter/services/MealTypeService.java
@@ -0,0 +1,16 @@
+package com.greenfox.caloriecounter.services;
+
+import com.greenfox.caloriecounter.repos.MealTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Anna on 17/01/10.
+ */
+@Service
+public class MealTypeService {
+
+    @Autowired
+    private MealTypeRepository repository;
+    
+}
