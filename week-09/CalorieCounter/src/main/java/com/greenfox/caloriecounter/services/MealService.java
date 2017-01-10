349610b1ddc9361a64a7b365d6069bca52179332
Anna812 <88menyus@gmail.com>
2017-01-10 12:01:05 +0100

    rename MealServices to the not plural version

---
+++ week-09/CalorieCounter/src/main/java/com/greenfox/caloriecounter/services/MealService.java
@@ -0,0 +1,21 @@
+package com.greenfox.caloriecounter.services;
+
+import com.greenfox.caloriecounter.models.Meal;
+import com.greenfox.caloriecounter.repos.MealRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * Created by Anna on 17/01/10.
+ */
+@Service
+public class MealService {
+
+    @Autowired
+    MealRepository repository;
+
+    public Iterable<Meal> list() {
+        return repository.findAll();
+    }
+}
--- week-09/CalorieCounter/src/main/java/com/greenfox/caloriecounter/services/MealServices.java
+++
@@ -1,7 +0,0 @@
-package com.greenfox.caloriecounter.services;
-
-/**
- * Created by Anna on 17/01/10.
- */
-public class MealServices {
-}
