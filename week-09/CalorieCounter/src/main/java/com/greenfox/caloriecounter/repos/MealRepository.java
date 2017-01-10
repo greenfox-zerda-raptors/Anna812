7c786621808b15ddfbd23039fa30e208a405fe2c
Anna812 <88menyus@gmail.com>
2017-01-10 20:00:05 +0100

    add method to MealRepository that lists all Meals into a Page

--- week-09/CalorieCounter/src/main/java/com/greenfox/caloriecounter/repos/MealRepository.java
+++ week-09/CalorieCounter/src/main/java/com/greenfox/caloriecounter/repos/MealRepository.java
@@ -1,10 +1,14 @@
 package com.greenfox.caloriecounter.repos;
 
 import com.greenfox.caloriecounter.models.Meal;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+
 
 /**
  * Created by Anna on 17/01/10.
  */
 public interface MealRepository extends CrudRepository<Meal, Long> {
+    Page<Meal> findAll(Pageable pageable);
 }
