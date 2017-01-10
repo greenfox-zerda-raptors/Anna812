ba236114f5eb71d5fd2c4c1e37eb7cea1356ed43
Anna812 <88menyus@gmail.com>
2017-01-10 11:21:25 +0100

    add field and noArgsConstructor for Meal class

--- week-09/CalorieCounter/src/main/java/com/greenfox/caloriecounter/models/Meal.java
+++ week-09/CalorieCounter/src/main/java/com/greenfox/caloriecounter/models/Meal.java
@@ -1,7 +1,29 @@
 package com.greenfox.caloriecounter.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
 /**
  * Created by Anna on 17/01/10.
  */
+@Entity
+@Table(name = "meals")
+@Getter
+@Setter
 public class Meal {
+    @Id
+    @GeneratedValue
+    private long id;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created;
+    private String type;
+    private String description;
+    private int calories;
+
+    public Meal() {
+        created = new Date();
+    }
 }
