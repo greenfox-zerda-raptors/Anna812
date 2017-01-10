ac3a20474fe2d5a5bb4ed5b71dcfb6692f9431c8
Anna812 <88menyus@gmail.com>
2017-01-10 12:03:17 +0100

    extend MealRepository from CrudRepository

--- week-09/CalorieCounter/src/main/java/com/greenfox/caloriecounter/repos/MealRepository.java
+++ week-09/CalorieCounter/src/main/java/com/greenfox/caloriecounter/repos/MealRepository.java
@@ -1,7 +1,10 @@
 package com.greenfox.caloriecounter.repos;
 
+import com.greenfox.caloriecounter.models.Meal;
+import org.springframework.data.repository.CrudRepository;
+
 /**
  * Created by Anna on 17/01/10.
  */
-public interface MealRepository {
+public interface MealRepository extends CrudRepository<Meal, Long> {
 }
