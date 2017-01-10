1482e9eb4813e7e3e940efe93a69cd0e02b46071
Anna812 <88menyus@gmail.com>
2017-01-10 12:03:47 +0100

    create controller method for index endpoint

--- week-09/CalorieCounter/src/main/java/com/greenfox/caloriecounter/controllers/CalorieCounterController.java
+++ week-09/CalorieCounter/src/main/java/com/greenfox/caloriecounter/controllers/CalorieCounterController.java
@@ -1,10 +1,23 @@
 package com.greenfox.caloriecounter.controllers;
 
+import com.greenfox.caloriecounter.services.MealService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by Anna on 17/01/10.
  */
 @Controller
 public class CalorieCounterController {
+
+    @Autowired
+    MealService mealService;
+
+    @RequestMapping("/index")
+    public String index(Model model) {
+        model.addAttribute("meals", mealService.list());
+        return "index";
+    }
 }
