720a53674c0787c7d309cb90ca5b2846116ba38a
Anna812 <88menyus@gmail.com>
2017-01-10 20:00:46 +0100

    add method to MealService that returns a Page object

--- week-09/CalorieCounter/src/main/java/com/greenfox/caloriecounter/services/MealService.java
+++ week-09/CalorieCounter/src/main/java/com/greenfox/caloriecounter/services/MealService.java
@@ -3,8 +3,9 @@ package com.greenfox.caloriecounter.services;
 import com.greenfox.caloriecounter.models.Meal;
 import com.greenfox.caloriecounter.repos.MealRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
-
 
 /**
  * Created by Anna on 17/01/10.
@@ -17,6 +18,10 @@ public class MealService {
 
     public Iterable<Meal> list() {
         return repository.findAll();
+    }
+
+    public Page<Meal> listPage(int pageNumber, int limit) {
+        return repository.findAll(new PageRequest(pageNumber, limit));
     }
 
     public void saveMeal(Meal meal) {
