47eaacc967720b8079ba0f004e87631d2bdbdce6
Anna812 <88menyus@gmail.com>
2017-02-07 11:31:11 +0100

    add binarySearch method to BinarySearch class

--- week-15/Algorithms/src/BinarySearch.java
+++ week-15/Algorithms/src/BinarySearch.java
@@ -2,4 +2,29 @@
  * Created by Anna on 17/02/07.
  */
 public class BinarySearch {
+    public static void main(String[] args) {
+        int[] list = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+        try {
+            int result = binarySearch(list, 2);
+            System.out.println(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int binarySearch(int[] list, int key) throws Exception {
+        int lowest = 0;
+        int highest = list.length - 1;
+        while(lowest<=highest) {
+            int middle = lowest + (highest - lowest) / 2;
+            if (key < list[middle]) {
+                highest = middle - 1;
+            } else if (key > list[middle]) {
+                lowest = middle + 1;
+            } else {
+                return middle + 1;
+            }
+        }
+        throw new Exception("The list does not contain given number");
+    }
 }
