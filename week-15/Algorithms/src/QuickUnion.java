7ae09ebaae1248e7f58f86f294f87047e32fba10
Anna812 <88menyus@gmail.com>
2017-02-07 17:01:32 +0100

    add methods to QuickUnion from video

--- week-15/Algorithms/src/QuickUnion.java
+++ week-15/Algorithms/src/QuickUnion.java
@@ -2,4 +2,30 @@
  * Created by Anna on 17/02/07.
  */
 public class QuickUnion {
+
+    private int[] id;
+
+    public QuickUnion(int N) {
+        id = new int[N];
+        for(int i =  0; i < N; i++) {
+            id[i] = i;
+        }
+    }
+
+    private int root(int input) {
+        while(input != id[input]) {
+            input = id[input];
+        }
+        return input;
+    }
+
+    public boolean connected (int pointA, int pointB) {
+        return root(pointA) == root(pointB);
+    }
+
+    public void union(int pointA, int pointB) {
+        int i = root(pointA);
+        int j = root(pointB);
+        id[i] = j;
+    }
 }
