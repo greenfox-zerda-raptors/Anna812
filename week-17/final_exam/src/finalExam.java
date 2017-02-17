3dbc6fd55d0a8fb92103e915333477ec697e339d
Anna812 <88menyus@gmail.com>
2017-02-17 10:01:50 +0100

    do the same functionality with if statements

--- week-17/final_exam/src/finalExam.java
+++ week-17/final_exam/src/finalExam.java
@@ -6,7 +6,13 @@ public class finalExam {
         int[] first = new int[]{1, 2, 3};
         int[] second = new int[]{6, 7, 8};
         for (int temp : mixTwoArrays(first, second)) {
-            System.out.println(temp);
+            System.out.print(temp + ", ");
+        }
+
+        System.out.println();
+
+        for (int temp : mixArrays(first, second)) {
+            System.out.print(temp + ", ");
         }
 
     }
@@ -18,6 +24,20 @@ public class finalExam {
             result[j] = one[i];
             result[ j + 1 ] = two[i];
             j += 2;
+        }
+        return result;
+    }
+
+    public static int[] mixArrays(int[] one, int[] two) {
+        int[] result =  new int[one.length + two.length];
+        int j = 0;
+        for(int i = 0; i < result.length; i++) {
+            if(i % 2 == 0) {
+                result[i] = one[j];
+            } else {
+                result[i] = two[j];
+                j++;
+            }
         }
         return result;
     }
