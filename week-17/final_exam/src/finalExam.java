860fd52e2380ccd8360415546bd940dbc54a135f
Anna812 <88menyus@gmail.com>
2017-02-17 09:24:36 +0100

    create main function, with 2 int arrays, and a function for mixing them into one array

--- week-17/final_exam/src/finalExam.java
+++ week-17/final_exam/src/finalExam.java
@@ -3,6 +3,13 @@
  */
 public class finalExam {
     public static void main(String[] args) {
+        int[] first = new int[]{1, 2, 3};
+        int[] second = new int[]{6, 7, 8};
 
+    }
+
+    public int[] mixTwoArrays(int[] one, int[] two) {
+
+        return null;
     }
 }
