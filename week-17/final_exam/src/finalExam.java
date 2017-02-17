e80ec13a89b8ecb522e69c7f6d8113d433a60d1c
Anna812 <88menyus@gmail.com>
2017-02-17 09:35:45 +0100

    first attempt to write mixing function

--- week-17/final_exam/src/finalExam.java
+++ week-17/final_exam/src/finalExam.java
@@ -5,11 +5,18 @@ public class finalExam {
     public static void main(String[] args) {
         int[] first = new int[]{1, 2, 3};
         int[] second = new int[]{6, 7, 8};
+        for (int temp : mixTwoArrays(first, second)) {
+            System.out.println(temp);
+        }
 
     }
 
-    public int[] mixTwoArrays(int[] one, int[] two) {
-
-        return null;
+    public static int[] mixTwoArrays(int[] one, int[] two) {
+        int[] result =  new int[one.length + two.length];
+        for (int i = 0; i < one.length; i++){
+            result[i] = one[i];
+            result[ i + 1 ] = two[i];
+        }
+        return result;
     }
 }
