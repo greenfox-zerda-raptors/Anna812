29941648ee390ce83ef9ad91cf8d7890df511239
Anna812 <88menyus@gmail.com>
2017-02-21 16:29:00 +0100

    create Euler03 for problem 3 about finding prime factors

---
+++ week-18/Algorithm_Practice/src/Euler03.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Anna on 17/02/21.
+ */
+public class Euler03 {
+    public static void main(String[] args) {
+
+    }
+
+    private static void primeFactor(int input) {
+        
+    }
+}
