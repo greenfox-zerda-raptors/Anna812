8cfe3f762522f838c73bfb533aeed03957dca40a
Anna812 <88menyus@gmail.com>
2017-02-21 16:39:34 +0100

    implement primeFactor method

--- week-18/Algorithm_Practice/src/Euler03.java
+++ week-18/Algorithm_Practice/src/Euler03.java
@@ -1,12 +1,23 @@
+import java.util.ArrayList;
+
 /**
  * Created by Anna on 17/02/21.
  */
 public class Euler03 {
     public static void main(String[] args) {
-
+        for (int temp : primeFactor(600851475143L)) {
+            System.out.println(temp);
+        }
     }
 
-    private static void primeFactor(int input) {
-        
+    private static ArrayList<Integer> primeFactor(long input) {
+        ArrayList<Integer> factors = new ArrayList<>();
+        for (int i = 2; i <= input; i++) {
+            while (input % i == 0) {
+                factors.add(i);
+                input /= i;
+            }
+        }
+        return factors;
     }
 }
